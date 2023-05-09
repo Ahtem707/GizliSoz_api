d77bb39cbe2e622db2a9daadf2a8948b0b10ad62
Ahtem Sitjalilov <akhtem.sitzhalilov@iw-group.pro>
2023-05-09 23:37:41 +0300

    Обновил хост медиаресурсов

--- requests/wordSound.py
+++ requests/wordSound.py
@@ -3,46 +3,48 @@ from extensions.RequestFactory import *
 class WordSound:
     app = None
     request = None
+
+    mediaHost = "82.146.49.164:3002/media/"
     
     def __init__(self, app):
         self.app = app
         self.request = RequestFactory(app)
 
     def _getSoundUrl(self, id: int, voiceActor: str):
-        if (id == 0): url = "https://www.ahtem.ru:3002/media/meraba.wav"
-        elif (id == 1): url = "https://www.ahtem.ru:3002/media/selam.wav"
-        elif (id == 2): url = "https://www.ahtem.ru:3002/media/ayva.wav"
-        elif (id == 3): url = "https://www.ahtem.ru:3002/media/alma.wav"
-        elif (id == 4): url = "https://www.ahtem.ru:3002/media/armut.wav"
-        elif (id == 5): url = "https://www.ahtem.ru:3002/media/portaqal.wav"
-        elif (id == 6): url = "https://www.ahtem.ru:3002/media/qara.wav"
-        elif (id == 7): url = "https://www.ahtem.ru:3002/media/aq.wav"
-        elif (id == 8): url = "https://www.ahtem.ru:3002/media/al.wav"
-        elif (id == 9): url = "https://www.ahtem.ru:3002/media/sari.wav"
-        elif (id == 10): url = "https://www.ahtem.ru:3002/media/mavi.wav"
-        elif (id == 11): url = "https://www.ahtem.ru:3002/media/esil.wav"
-        elif (id == 12): url = "https://www.ahtem.ru:3002/media/qursaq.wav"
-        elif (id == 13): url = "https://www.ahtem.ru:3002/media/saqal.wav"
-        elif (id == 14): url = "https://www.ahtem.ru:3002/media/qulaq.wav"
-        elif (id == 15): url = "https://www.ahtem.ru:3002/media/tirnaq.wav"
-        elif (id == 16): url = "https://www.ahtem.ru:3002/media/tis.wav"
-        elif (id == 17): url = "https://www.ahtem.ru:3002/media/turaq.wav"
-        elif (id == 18): url = "https://www.ahtem.ru:3002/media/qaplan.wav"
-        elif (id == 19): url = "https://www.ahtem.ru:3002/media/qoy.wav"
-        elif (id == 20): url = "https://www.ahtem.ru:3002/media/at.wav"
-        elif (id == 21): url = "https://www.ahtem.ru:3002/media/qoyan.wav"
-        elif (id == 22): url = "https://www.ahtem.ru:3002/media/jirafe.wav"
-        elif (id == 23): url = "https://www.ahtem.ru:3002/media/fil.wav"
-        elif (id == 24): url = "https://www.ahtem.ru:3002/media/deve.wav"
-        elif (id == 25): url = "https://www.ahtem.ru:3002/media/narguz.wav"
-        elif (id == 26): url = "https://www.ahtem.ru:3002/media/lale.wav"
-        elif (id == 27): url = "https://www.ahtem.ru:3002/media/gul.wav"
-        elif (id == 28): url = "https://www.ahtem.ru:3002/media/ahudut.wav"
-        elif (id == 29): url = "https://www.ahtem.ru:3002/media/qavun.wav"
-        elif (id == 30): url = "https://www.ahtem.ru:3002/media/dut.wav"
+        if (id == 0): url = "meraba.wav"
+        elif (id == 1): url = "selam.wav"
+        elif (id == 2): url = "ayva.wav"
+        elif (id == 3): url = "alma.wav"
+        elif (id == 4): url = "armut.wav"
+        elif (id == 5): url = "portaqal.wav"
+        elif (id == 6): url = "qara.wav"
+        elif (id == 7): url = "aq.wav"
+        elif (id == 8): url = "al.wav"
+        elif (id == 9): url = "sari.wav"
+        elif (id == 10): url = "mavi.wav"
+        elif (id == 11): url = "esil.wav"
+        elif (id == 12): url = "qursaq.wav"
+        elif (id == 13): url = "saqal.wav"
+        elif (id == 14): url = "qulaq.wav"
+        elif (id == 15): url = "tirnaq.wav"
+        elif (id == 16): url = "tis.wav"
+        elif (id == 17): url = "turaq.wav"
+        elif (id == 18): url = "qaplan.wav"
+        elif (id == 19): url = "qoy.wav"
+        elif (id == 20): url = "at.wav"
+        elif (id == 21): url = "qoyan.wav"
+        elif (id == 22): url = "jirafe.wav"
+        elif (id == 23): url = "fil.wav"
+        elif (id == 24): url = "deve.wav"
+        elif (id == 25): url = "narguz.wav"
+        elif (id == 26): url = "lale.wav"
+        elif (id == 27): url = "gul.wav"
+        elif (id == 28): url = "ahudut.wav"
+        elif (id == 29): url = "qavun.wav"
+        elif (id == 30): url = "dut.wav"
         
         else: url = None
-        return url
+        return self.mediaHost + url
 
     def wordSound(self, request):
         if request.method != 'GET':
