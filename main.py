a31669da751ff98aa8199450bcb3f59f009e7b9c
Ahtem Sitjalilov <a.sitjalilov@ideas-world.com>
2023-05-15 02:04:45 +0300

    Поправил примеры запросов, удалил лишние

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask import request
 from requests.system import *
 from requests.level import *
-from requests.crossword import *
-from requests.wordSound import *
 
 app = Flask(__name__)
 
@@ -26,14 +24,6 @@ def getLevelRequest():
 @app.route('/getLevels')
 def getLevelsRequest():
     return Level(app).getLevels(request)
-
-@app.route('/crosswords')
-def crosswordsRequest():
-    return Crosswords(app).crosswords(request)
-
-@app.route('/wordSound')
-def wordSoundRequest():
-    return WordSound(app).wordSound(request)
 
 if __name__ == '__main__':
     # app.run(host='82.146.49.164', port=8005, debug = True)
